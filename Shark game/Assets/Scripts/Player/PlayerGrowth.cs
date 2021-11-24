daed404b588f80bb983a19f542f1e0ccbe0a5de4
LarsenLasse <lasselarsen00@gmail.com>
2021-11-24 15:27:25 +0100

    EXP

--- Shark game/Assets/Scripts/Player/PlayerGrowth.cs
+++ Shark game/Assets/Scripts/Player/PlayerGrowth.cs
@@ -1,18 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class PlayerGrowth : MonoBehaviour
 {
     // Start is called before the first frame update
+    public Level level;
     void Start()
     {
-        
+        level = new Level(1, OnLevelUp);
+
+    }
+
+    public void OnLevelUp()
+    {
+        print("I leveled up!");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            level.AddExp(100);
+            
+        }
     }
 }
