e550ad04f8c83ee0ad5094eb4ef911726b2d952c
thederpygrandma <andreas.liebach@gmail.com>
2021-11-25 11:19:16 +0100

    kamera zoom der ikke. virker

---
+++ Shark game/Assets/Scripts/Player/CameraZoom.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class CameraZoom : MonoBehaviour
+{
+    // Movement based Scroll Wheel Zoom.
+
+    public Transform parentObject;
+    public float zoomLevel;
+    public float sensitivity=1;
+    public float speed = 30;
+    public float maxZoom=1000000;
+    float zoomPosition;
+
+    void Update()
+    {
+        zoomLevel += Input.mouseScrollDelta.y * sensitivity;
+        zoomLevel = Mathf.Clamp(zoomLevel, 0, maxZoom);
+        zoomPosition = Mathf.MoveTowards(zoomPosition, zoomLevel, speed * Time.deltaTime);
+        transform.position = parentObject.position + (transform.forward * zoomPosition);
+    }
+    
+}
