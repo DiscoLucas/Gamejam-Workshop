088591a062d93967b8b052203d2651ed09bc0da1
LarsenLasse <lasselarsen00@gmail.com>
2021-11-25 15:53:26 +0100

    SharkBullet

--- Shark game/Assets/Scripts/Player/SharkBullet.cs
+++ Shark game/Assets/Scripts/Player/SharkBullet.cs
@@ -7,7 +7,7 @@ public class SharkBullet : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Enemy"))
+        if (other.gameObject.tag=="Enemy")
         {
             Destroy(other.gameObject);
         }
